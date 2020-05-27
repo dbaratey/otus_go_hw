5843d885f30bc36f31cad9568dfd0c3e948a6067
Barada <barateydn@bzaem.com>
2020-05-27 17:33:45 +0300

    add files

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	ntpTime, err := ntp.Time("ntp1.stratum2.ru")
+	if err != nil {
+		log.Fatal(err)
+	}
+	nowTime := time.Now()
+	fmt.Printf("current time: %s\nexact time: %s\n", nowTime.Round(0), ntpTime.Round(0))
 }
